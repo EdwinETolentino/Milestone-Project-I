8e04b4b6bf3acc654b37a75e8bae2f6f61b92b9e
Edwin Tolentino <edwintolentino@Edwins-Macbook.local>
2023-04-04 23:37:57 -0700

    modified

--- add.js
+++ add.js
@@ -1,1 +1,16 @@
 let username = window.prompt ("Welcome! What is your name?")
+
+let answerKey = [
+    ["Pennsylvania_in_1681", "William_Penn"], 
+    ["1st_Baron_Baltimore", "North_American_province_of_Maryland"],
+    ["No_Taxation", "Representation"],
+    ["Plymouth_Colony", "Massachusetts"],
+    ["Austrian_Succession", "King_George's_War"],
+    ["Establish_Colonies", "French"],
+    ["English_settlement", "Jamestown"],
+    ["Indigenous_peoples", "Thanksgiving"],
+    ["Worth_Of_Trinkets", "Peter_Minuit"],
+    ["University_Of", "Philadelphia"]
+]
+
+
